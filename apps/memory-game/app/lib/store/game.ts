be0b9b9aa2b4f1076d54483314d86bbc416ee1aa
Grzegorz Mandziak <gmandziak@membr.com>
2025-03-14 16:49:45 +0100

    remove console logs

--- apps/memory-game/app/lib/store/game.ts
+++ apps/memory-game/app/lib/store/game.ts
@@ -21,23 +21,18 @@ export const useGameStore = create<GameStore>((set, get) => ({
   flippedIndices: [],
   actions: {
     flipCard: (index: number) => {
-      console.log("🃏flipCard", index);
       const { flippedIndices, cards, timeout, level } = get();
       const revealTime = levelToRevealTime[level] * 1000;
 
-      console.log("flippedIndices", flippedIndices);
       if (flippedIndices.includes(index)) {
-        console.log("card already flipped", flippedIndices);
         return;
       }
 
       if (timeout) {
-        console.log("clearing timeout");
         clearTimeout(timeout);
       }
 
       if (flippedIndices.length === 0) {
-        console.log("first card flipped");
         set((state) => ({
           flippedIndices: [...state.flippedIndices, index],
           cards: state.cards.map((card, i) =>
@@ -53,14 +48,11 @@ export const useGameStore = create<GameStore>((set, get) => ({
       }
 
       if (flippedIndices.length === 1) {
-        console.log("second card flipped");
         const [firstIndex] = flippedIndices;
         const firstCard = cards[firstIndex];
         const secondCard = cards[index];
 
         if (firstCard.emoji === secondCard.emoji) {
-          console.log("cards matching");
-
           set((state) => ({
             flippedIndices: [],
             cards: state.cards.map((card, i) =>
@@ -71,7 +63,6 @@ export const useGameStore = create<GameStore>((set, get) => ({
             timeout: null,
           }));
         } else {
-          console.log("cards not matching");
           set((state) => ({
             flippedIndices: [...state.flippedIndices, index],
             cards: state.cards.map((card, i) =>
@@ -92,7 +83,6 @@ export const useGameStore = create<GameStore>((set, get) => ({
       }
 
       if (flippedIndices.length >= 2) {
-        console.log("3rd card flipped, clearing");
         set((state) => ({
           flippedIndices: [index],
           cards: state.cards.map((card, i) => {
@@ -112,7 +102,7 @@ export const useGameStore = create<GameStore>((set, get) => ({
                 i === index ? { ...card, isFlipped: false } : card,
               ),
             }));
-          }, revealTime),
+          }, revealTime / 2),
         }));
       }
     },
